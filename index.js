0b2c05c3062a62a897b01e8fa12e39abb92a0c96
ppmasa8 <kiyokiyopip56@gmail.com>
2021-04-23 18:46:43 +0900

    add initial function

--- index.js
+++ index.js
@@ -14,9 +14,34 @@ const line_config = {
 // webサーバー設定
 server.listen(process.env.PORT || 3000);
 
+// APIコールのためのクライアントインスタンスを作成
+const bot = new line.Client(line_config);
+
 // ---------------------------------------------------------------------------------------------------------------------
 // ルーター設定
 server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
+    // 先行してLINE側にステータスコード200でレスポンスする。
     res.sendStatus(200);
-    console.log(req.body);
+
+    // すべてのイベント処理のプロミスを格納する配列。
+    let events_processed = [];
+
+    // イベント処理のハンドリング
+    req.body.events.forEach((event) => {
+        if (event.type == "message" && event.message.type == "test"){
+            if (event.message.text == "ガチホコ"){
+                events_processed.push(bot.replyMessage(event.replyToken, {
+                    type: "text",
+                    text: "二度とやりたくないゴミルール"
+                }));
+            }
+        }
+    });
+
+    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
+    Promise.all(events_processed).then(
+        (response) => {
+            console.log(`${response.length} event(s) processed.`);
+        }
+    );
 });
